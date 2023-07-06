2aac8e76803705f887892aad8ef1f5a67acaed40
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-07-06 00:09:04 -0300

    criando funcao de alterar estagio do game

--- gameofwords/src/App.js
+++ gameofwords/src/App.js
@@ -1,10 +1,46 @@
 import './App.css';
+
+//react
+import { useCallback, useEffect, useState } from "react";
+
+//data
+import {wordsList} from "./data/words"
+
+//components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  //starts the game of word
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  //reiniciar o game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
