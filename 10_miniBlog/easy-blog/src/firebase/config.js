03563c8dc0fce96d872a02a1cdda1c45bafcb44b
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-07-12 21:35:12 -0300

    Inicio projeto blog, configurações iniciais

---
+++ 10_miniBlog/easy-blog/src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUEwb6e8B1vs-yYSCL3UI6jdcSbYydK5I",
+  authDomain: "mini-blog-ef090.firebaseapp.com",
+  projectId: "mini-blog-ef090",
+  storageBucket: "mini-blog-ef090.appspot.com",
+  messagingSenderId: "431699462138",
+  appId: "1:431699462138:web:14b0b161ab7cf36a994643"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFireStore(app)
+
+export { db };
