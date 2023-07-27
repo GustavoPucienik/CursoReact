0568883e0a738ccba3e85cf001e0d9e91da659b4
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-07-26 21:50:46 -0300

    Termino do projeto filmes info

--- 13_projeto_filmes/movies.info/src/App.jsx
+++ 13_projeto_filmes/movies.info/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
      <div className='App'>
      <Navbar />
-      <h2>Movies lib</h2>
       <Outlet/>
      </div>
     </>
